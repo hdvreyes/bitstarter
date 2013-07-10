857a1e05799a269d9c4fa2e9eb8fd3ffc5cbfcea
hdvreyes <hdvreyes@gmail.com>
2013-07-10 18:07:54 +0800

    new commit again

--- web.js
+++ web.js
@@ -2,21 +2,26 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+app.get('/', function(request, response) {
 
-var page;
+		var page = fs.readFileSync('index.html', 'utf8');
+	  	response.send(page);
 
-try {
-  page = fs.readFileSync('index.html');
-  response.send( page );
-} catch (e) {
-  	// Here you get the error when the file was not found,
-  	// but you also get any other error
-	if (e.code === 'ENOENT') {
-	  console.log('File not found!');
-	} else {
-	  throw e;
-	}  
-}
+});
+
+
+// var data;
+// try {
+//   data = fs.readFileSync('foo.bar');
+// } catch (e) {
+//   // Here you get the error when the file was not found,
+//   // but you also get any other error
+// 	if (e.code === 'ENOENT') {
+// 	  console.log('File not found!');
+// 	} else {
+// 	  throw e;
+// 	}  
+// }
 
 
 var port = process.env.PORT || 5000;
