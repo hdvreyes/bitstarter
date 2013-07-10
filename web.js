fc3aa9d30cdb87d660454c7e8343415cf494db78
hdvreyes <hdvreyes@gmail.com>
2013-07-10 10:10:15 +0000

    commit me

--- web.js
+++ web.js
@@ -2,21 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+app.readFileSync('index.html', 'utf8');
 
-var page;
-
-try {
-  page = fs.readFileSync('index.html');
-  response.send( page );
-} catch (e) {
-  	// Here you get the error when the file was not found,
-  	// but you also get any other error
-	if (e.code === 'ENOENT') {
-	  console.log('File not found!');
-	} else {
-	  throw e;
-	}  
-}
 
 
 var port = process.env.PORT || 5000;
