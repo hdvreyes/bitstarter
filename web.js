a430f1ee0229badd33462a9b40dd07396504a38e
hdvreyes <hdvreyes@gmail.com>
2013-07-10 09:23:55 +0000

    return web.js to normal

--- web.js
+++ web.js
@@ -2,14 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-// app.get('/', function(request, response) {
-//   response.send('Hello World!');
-// });
+ app.get('/', function(request, response) {
+   response.send('Hello World!');
+ });
 
-fs.writeFile('index.html', 'Test', function (err) {
-  if (err) throw err;
-  console.log('It\'s saved!');
-});
+//fs.writeFile('index.html', 'Test', function (err) {
+//  if (err) throw err;
+//  console.log('It\'s saved!');
+//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
