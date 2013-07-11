35bed8a56679154a0d3ea88fc14b1343fe8ec608
hdvreyes <hdvreyes@gmail.com>
2013-07-11 19:00:01 +0800

    test 3

--- grader.js
+++ grader.js
@@ -26,7 +26,7 @@ var program             = require('commander');
 var cheerio             = require('cheerio');
 var HTMLFILE_DEFAULT    = "index.html";
 var CHECKSFILE_DEFAULT  = "checks.json";
-
+var HTMLURL_DEFAULT    = "http://serene-shore-5970.herokuapp.com/";
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -65,8 +65,9 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_url>', 'Path to url', clone(assertFileExists), HTMLURL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var checkJson = checkHtmlFile(program.file, program.checks, program.url);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
